c26dda742c613849d689f427e1f76fb00a6af035
Alezuna Zikri <alezunazikri@My-Pro-M1.local>
2024-01-15 14:07:18 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        x : "#52525b",
+        z : "#ded2a4"
+      },
+    },
+  },
+  plugins: [],
+}
+
