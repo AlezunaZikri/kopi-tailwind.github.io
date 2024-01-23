bacb3e607418fda1978d5cfddcb49867af91791c
Alezuna Zikri <alezunazikri@My-Pro-M1.local>
2024-01-23 10:27:58 +0700

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,52 @@
 // navbar fixed
-window.onscroll = function(){
+window.onscroll = function () {
     const header = document.querySelector('header');
     const navbarFixed = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
-    if(window.pageYOffset){
+    if (window.pageYOffset) {
         header.classList.add('navbar-fixed');
-    }else{
-        navbarFixed.classList.remove('navbar-fixed')
+        toTop.classList.remove('hidden');
+    } else {
+        navbarFixed.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
     }
 }
 
 // button humberger
-const humberger =document.querySelector('#humberger');
-const navMenu =document.querySelector('#nav-menu');
+const humberger = document.querySelector('#humberger');
+const navMenu = document.querySelector('#nav-menu');
 
-humberger.addEventListener('click', function(){
+humberger.addEventListener('click', function () {
     humberger.classList.toggle('active');
     navMenu.classList.toggle('hidden');
-})
+});
+
+// klik dimanapun
+window.addEventListener('click', function (a) {
+    if (a.target != humberger && a.target != navMenu) {
+        humberger.classList.remove('active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// dark mode
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark')
+        localStorage.theme = 'light';
+    }
+});
+
+// perpindahan tombl
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
+}
